2b9dbe5ba63fa9cd25792e2b78bbc12ff2d10adc
stormrage <stormrage.qb@gmail.com>
2024-02-14 17:01:55 +0900

    feat: pagination and search in invoice page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
+
 export default async function Page() {
 
   return (
