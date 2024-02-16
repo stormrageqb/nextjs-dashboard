9efe4c12203e852aafd99e2d16f997a9fde93592
stormrage <stormrage.qb@gmail.com>
2024-02-16 17:01:54 +0900

    feat: validation

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -169,6 +169,8 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+      
+    console.log(invoice);
 
     return invoice[0];
   } catch (error) {
